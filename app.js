1d37daf42fd7f096b3fd3c727c21c0dddf8bb4e4
Anton Zhalybin <armagadon@mail.ru>
2018-08-15 19:20:50 +0700

    test1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var browserSync=require('browser-sync');
 /**кккк*/
+//TEST 1 1 1 1 1 1 11 1 1 1 1 1 1 1 1 1 1 1 1 1 1 11 1 1 1 1 1 1 1 1 1 1 1 1 1 
 browserSync({
 	server:"app",
 	files:["app/*.html", "app/*.css", "app/api/*.json", "app/js/*.js"]
